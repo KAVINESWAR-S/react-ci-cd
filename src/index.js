9a2924338706e0a63cab9649ee07e973ceb66d77
KAVINESWAR-S <kavineswars770@gmail.com>
2026-01-11 10:36:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css";
+function hello() {
+    console.log("Hello from React CI/CD Project");
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+hello();
